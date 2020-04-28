5b687425d3c246f60f1197fe82bdd93348b246e5
QuarkNerd <garora1410@yahoo.co.uk>
2020-04-28 21:40:19 +0100

    starup game in run

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-pub fn run() {
+use definitions::*;
 
+pub fn run() {
+    let one = HumanPlayer::new("Player one name:");
+    let two = HumanPlayer::new("Player two name:");
+    let mut game = normal_game::Game::from(one, two);
+    game.play_game();
 }
 
 mod normal_game;
