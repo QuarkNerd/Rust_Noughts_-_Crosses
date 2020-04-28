6b040babc5ba0737d6459c33962ef4c2a849cf09
QuarkNerd <garora1410@yahoo.co.uk>
2020-04-28 20:59:02 +0100

    create human players

--- src/definitions.rs
+++ src/definitions.rs
@@ -1,5 +1,33 @@
+use std::io;
+
 struct StatusUpdate {
     display_state: String,
     game_in_progress: bool,
-    possible_moves: Vec<String>,
+}
+
+struct HumanPlayer {
+    identity: String,
+}
+
+impl HumanPlayer {
+    fn give_update(&self, update: StatusUpdate) -> Option<String> {
+        println!("Message for {}", &self.identity);
+        println!("{}", update.display_state);
+        if update.game_in_progress {
+            Some(get_user_input("What is your next move?"))
+        } else {
+            None
+        }
+    }
+}
+
+fn get_user_input(prompt: &str) -> String {
+    println!("{}", prompt);
+
+    let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    guess
 }
