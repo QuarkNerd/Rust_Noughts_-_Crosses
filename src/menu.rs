8915e32b65722020b211bc1e9b1eac7b1003b352
Gurveer Arora <garora1410@yahoo.co.uk>
2020-07-28 12:33:55 +0100

    add loop and leave to menu

--- src/menu.rs
+++ src/menu.rs
@@ -1,9 +1,13 @@
 
 use crate::utilities::*;
+
+#[derive(Clone)]
 pub enum menu_option_action<'a, T> {
     sub_menu(Vec<menu_option<'a, T>>),
     callback(fn(state: T) -> T),
+    leave
 }
+#[derive(Clone)]
 pub struct menu_option<'a, T> {
     pub command: &'a str,
     pub description: &'a str,
@@ -12,23 +16,26 @@ pub struct menu_option<'a, T> {
 
 pub fn show_menu<'a, T>(options: &Vec<menu_option<&'a mut T>>, state: &'a mut T) -> &'a mut T{
     let mut state = state;
-    let mut prompt = "Please select from the following options \n\n".to_string();
+    loop {
+        let mut prompt = "Please select from the following options \n\n".to_string();
 
-    for menu_item in options.iter() {
-        prompt.push_str(&format!("{}: {} \n", &menu_item.command, &menu_item.description));
-    }
+        for menu_item in options.iter() {
+            prompt.push_str(&format!("{}: {} \n", &menu_item.command, &menu_item.description));
+        }
 
-    let mut input = String::new();
-    let mut selected_option: Option<&menu_option<&mut T>> = None;
+        let mut input = String::new();
+        let mut selected_option: Option<&menu_option<&mut T>> = None;
 
-    while selected_option.is_none() {
-        input = get_user_input(&prompt);
-        selected_option = options.iter().find(|&x| x.command == input);
-    }
+        while selected_option.is_none() {
+           input = get_user_input(&prompt);
+           selected_option = options.iter().find(|&x| x.command == input);
+        }
 
-    match &selected_option.unwrap().action {
-        menu_option_action::sub_menu(sub_menu) => state = show_menu(&sub_menu, state),
-        menu_option_action::callback(callback) => state = callback(state),
+        match &selected_option.unwrap().action {
+            menu_option_action::sub_menu(sub_menu) => state = show_menu(&sub_menu, state),
+            menu_option_action::callback(callback) => state = callback(state),
+            menu_option_action::leave => break,
+        }
     }
 
     return state
