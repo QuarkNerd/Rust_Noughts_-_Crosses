0aeab105b39bbc26a27537d3b43efc1c03a3a044
Gurveer Arora <garora1410@yahoo.co.uk>
2020-07-31 22:28:55 +0100

    move reading string from file to its own utility

--- src/utilities.rs
+++ src/utilities.rs
@@ -36,15 +36,19 @@ pub fn deserialize_from_relative_path<T>(file: PathBuf) -> T
     where 
         T: DeserializeOwned
 {
-    let mut path = current_dir().unwrap();
-    path.push(file);
-    let mut file = File::open(path).unwrap();
-    let mut serialized_string = String::new();
-    file.read_to_string(&mut serialized_string).unwrap();
+    let serialized_string = load_with_relative_path(file);
     let a: T = serde_json::from_str(&serialized_string).unwrap();
     a
 }
 
+pub fn load_with_relative_path(file: PathBuf) -> String {
+    let mut path = current_dir().unwrap();
+    path.push(file);
+    let mut file = File::open(path).unwrap();
+    let mut string = String::new();
+    file.read_to_string(&mut string).unwrap();
+    string
+}
 
 pub fn map_a_hash_map<T,U, V, W>(original: &HashMap<T,U>, key_value_mapper: fn((&T,&U)) -> (V,W) ) -> HashMap<V,W> 
     where 
